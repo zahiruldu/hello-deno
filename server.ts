13476301b19494d8bb1d60c6b03b4f9fac9ae412
zahiruldu <zahirul.haque@aurea.com>
2020-05-14 16:45:16 +0600

    DEV: Hello Deno

---
+++ server.ts
@@ -0,0 +1,18 @@
+import { Application } from "https://deno.land/x/oak/mod.ts";
+
+const app = new Application();
+
+// Logger
+app.use(async (ctx, next) => {
+  await next();
+  const rt = ctx.response.headers.get("X-Response-Time");
+  console.log(`${ctx.request.method} ${ctx.request.url} - ${rt}`);
+});
+
+
+// Hello World!
+app.use((ctx) => {
+  ctx.response.body = "Hello World!";
+});
+
+await app.listen({ port: 8000 });
